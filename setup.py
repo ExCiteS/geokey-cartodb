c9edfebc9c1446c279741fcf12b3100f8bfbc065
Julius O <j.osokinas@mappingforchange.org.uk>
2015-11-23 15:39:04 +0000

    Package setup improvement

--- setup.py
+++ setup.py
@@ -1,34 +1,21 @@
-import os
-del os.link
+#!/usr/bin/env python
 
-from distutils.core import setup
+from os.path import join
+from setuptools import setup, find_packages
+
+
+name = 'geokey-cartodb'
+version = __import__(name.replace('-', '_')).__version__
+repository = join('https://github.com/ExCiteS', name)
 
 setup(
-    # Application name:
-    name="geokey_cartodb",
-
-    description='Provides API endpoints for cartodb',
-
-    # Version number (initial):
-    version="0.1.0-beta-1",
-
-    # Application author details:
-    author="Oliver Roick",
-    author_email="o.roick@ucl.ac.uk",
-
-    # url='https://github.com/ExCiteS/geokey-cartodb', # use the URL to the github repo
-    # download_url='https://github.com/ExCiteS/geokey-cartodb/tarball/0.1-beta-2',
-
-    # Packages
-    packages=["geokey_cartodb"],
-
-    package_data={'geokey_cartodb': ['templates/*.html', 'migrations/*.py']},
-
-    # Include additional files into the package
+    name=name,
+    version=version,
+    description='Provides API endpoints for CartoDB',
+    url=repository,
+    download_url=join(repository, 'tarball', version),
+    author='Oliver Roick',
+    author_email='excitesucl@gmail.com',
+    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*']),
     include_package_data=True,
-
-    # Dependent packages (distributions)
-    install_requires=[
-
-    ],
 )
