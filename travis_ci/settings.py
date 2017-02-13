0a182ca7917284ad9d35b62c18a63ac4ca2368d0
Julius <j.osokinas@mappingforchange.org.uk>
2017-02-13 10:17:18 +0000

    New Travis CI configuration

--- travis_ci/settings.py
+++ travis_ci/settings.py
@@ -1,12 +1,12 @@
-"""GeoKey settings for Travis CI."""
+"""GeoKey settings."""
 
-import os.path
 from geokey.core.settings.dev import *
 
-DEFAULT_FROM_EMAIL = 'sender@example.com'
+
+DEFAULT_FROM_EMAIL = 'no-reply@travis-ci.org'
 ACCOUNT_EMAIL_VERIFICATION = 'optional'
 
-SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
 
 DATABASES = {
     'default': {
@@ -28,5 +28,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = normpath(join(dirname(dirname(abspath(__file__))), 'assets'))
 MEDIA_URL = '/assets/'
 
-WSGI_APPLICATION = 'settings.wsgi.application'
-ROOT_URLCONF = 'settings.urls'
+WSGI_APPLICATION = 'wsgi.application'
